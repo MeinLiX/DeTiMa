453cb16bcf64aa96a1f074279497ff65041bbcfe
meinlix <meinlix@ukr.net>
2021-05-13 15:27:41 +0300

    NEW: initial MainWindowViewModel and connected to MainWindow;

---
+++ DeTiMa/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+using DeTiMa.Utils.Commands;
+
+namespace DeTiMa.ViewModels
+{
+    class MainWindowViewModel
+    {
+        #region commands
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Commands
+
+            #endregion
+        }
+    }
+}
