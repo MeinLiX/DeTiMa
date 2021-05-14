bd6abdbfda14accfe1dd3d079bd505869958a69e
meinlix <meinlix@ukr.net>
2021-05-14 12:54:13 +0300

    NEW: initial controller for air db context;

---
+++ DeTiMa/Utils/DBControllers/Air/FlyTicketsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+using DeTiMa.Models.AirDB;
+
+namespace DeTiMa.Utils.DBControllers.Air
+{
+    class FlyTicketsController
+    {
+        private readonly DB_AirContext _airContext;
+
+        public FlyTicketsController(DB_AirContext airContext)
+        {
+            this._airContext = airContext;
+        }
+
+        internal async Task<List<AirTicket>> GetTickets() => await _airContext.AirTicket.ToListAsync();
+    }
+}
---
+++ DeTiMa/Utils/DBControllers/Air/MainController.cs
@@ -0,0 +1,10 @@
+﻿using DeTiMa.Models.AirDB;
+
+namespace DeTiMa.Utils.DBControllers.Air
+{
+    class MainController
+    {
+        private readonly static DB_AirContext _airContext = new();
+        internal static FlyTicketsController FlyTickets { get; } = new(_airContext);
+    }
+}
