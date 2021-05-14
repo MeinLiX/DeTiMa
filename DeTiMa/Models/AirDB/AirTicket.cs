a155c1b0cb83b60f543496b211850cbf96345a63
meinlix <meinlix@ukr.net>
2021-05-14 12:53:15 +0300

    NEW: initial db context with model airticket;

---
+++ DeTiMa/Models/AirDB/AirTicket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeTiMa.Models.AirDB
+{
+    public class AirTicket
+    {
+        [Key]
+        [Required]
+        public int Code { get; set; }
+
+        [Required]
+        [MaxLength(64, ErrorMessage = "The maximum length of the company field is 64")]
+        [MinLength(1, ErrorMessage = "The minimum length of the company field is 1")]
+        public string Company { get; set; }
+
+        [Required]
+        [RegularExpression(@"[a-zA-Z_ ]+\([A-Z]{3,4}\)", ErrorMessage = "Invalid airoport!")]
+        public string AirportOfDeparture { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DepartureTime { get; set; }
+
+        [Required]
+        [RegularExpression(@"[a-zA-Z_ ]+\([A-Z]{3,4}\)", ErrorMessage = "Invalid airoport!")]
+        public string AirportOfArrival { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime ArrivalTime { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DeTiMa/Models/AirDB/DB_AirContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeTiMa.Models.AirDB
+{
+    public class DB_AirContext : DbContext
+    {
+        public DbSet<AirTicket> AirTicket { get; set; }
+
+        public DB_AirContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@$"Filename={Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + @"\Air.db"}");
+        }
+    }
+}
